f64462468f5499d5c4c91583da50188a26a576ce
Furio Alessio <alefurio@hotmail.it>
2020-10-12 17:02:54 +0200

    generatore password con dichiarazione di variabile che contiene tutti i dati input dell' utente

--- scripts.js
+++ scripts.js
@@ -12,5 +12,12 @@ var colore_preferito = prompt ("Qual'è il tuo colore preferito?");
 console.log (colore_preferito);
 document.getElementById ('colore_preferito').innerHTML = document.getElementById ('colore_preferito').innerHTML + colore_preferito;
 
+
+//stampa a video e in console della password senza la dichiarazione di una variabile contenitore di tutti gli input dell' utente
 console.log ('la tua password è: ' + nome_utente + cognome_utente + colore_preferito + 20);
 document.getElementById ('password').innerHTML = document.getElementById ('password').innerHTML + nome_utente + cognome_utente + colore_preferito + 20;
+
+
+//stampa a video della password con la dichiarazione di una variabile "password_stampa_a_video" che contenga tutti gli input dell'utente
+var password_stampa_a_video = nome_utente + cognome_utente + colore_preferito + 20;
+document.getElementById ('password_stampa_a_video').innerHTML = document.getElementById ('password_stampa_a_video').innerHTML + password_stampa_a_video;
