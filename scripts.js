2f7ba6dd31f4687be21e7da9295cb5921190c54b
Furio Alessio <alefurio@hotmail.it>
2020-10-12 16:28:42 +0200

    generatore password con console.log()

---
+++ scripts.js
@@ -0,0 +1,12 @@
+alert('Benvenuto');
+
+var nome_utente = prompt ("Qual'è il tuo nome?");
+console.log (nome_utente);
+
+var cognome_utente = prompt ("Qual'è il tuo cognome?");
+console.log (cognome_utente);
+
+var colore_preferito = prompt ("Qual'è il tuo colore preferito?");
+console.log (colore_preferito);
+
+console.log ('la tua password è: ' + nome_utente + cognome_utente + colore_preferito + 20);
